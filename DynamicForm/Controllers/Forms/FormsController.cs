11b57f4c45153a3ccb73d8236b78381ec5582b49
Silpa Gunturu <silpa.gunturu@criticalriver.com>
2023-06-20 14:50:44 +0530

    New changes

---
+++ DynamicForm/Controllers/Forms/FormsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicForm.Controllers.Forms
+{
+    public class FormsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DynamicForm/Models/Forms/Form.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicForm.Models.Forms
+{
+    public class Form
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public int Ordinal { get; set; }
+    }
+}

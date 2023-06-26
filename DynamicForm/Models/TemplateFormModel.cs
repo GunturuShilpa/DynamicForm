d0f8b96151605c18eb84ea9ade760ab702a5f307
Jhansi Panneru <jhansi.panneru@criticalriver.com>
2023-06-26 15:59:05 +0530

    Edit Form Validations

--- DynamicForm/Models/TemplateFormModel.cs
+++ DynamicForm/Models/TemplateFormModel.cs
@@ -1,9 +1,11 @@
-﻿namespace DynamicForm.Models
+﻿using Microsoft.Build.Framework;
+
+namespace DynamicForm.Models
 {
     public class TemplateFormModel
     {
         public int Id { get; set; }
-
+        [Required]
         public string Name { get; set; } = string.Empty;
 
         public string Description { get; set; } = string.Empty;
